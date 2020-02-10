7c7dc781cf567f58abc0acc3323389668d5131c5
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2020-02-10 00:21:14 -0300

    adding the initial scope of the project

--- src/index.js
+++ src/index.js
@@ -8,6 +8,104 @@ dotenv.config()
 const app = express()
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    const help = `
+    <pre>
+    ## Requirements
+    
+    # Authentication
+      I can use an Authorization header to work with the data:
+      fetch(url, { headers: { 'Authorization': 'any toke' }})
+  
+    # Build the following endpoints:
+  
+      GET /categories
+        USAGE:
+          Get all of the categories available for the app. List is found in (categories.js.)
+  
+      GET /:category/posts
+        USAGE:
+          Get all of the posts for a particular category
+  
+      GET /posts
+        USAGE:
+          Get all of the posts. Useful for the main page when no category is selected.
+  
+      POST /posts
+        USAGE:
+          Add a new post
+  
+        PARAMS:
+          id - UUID should be fine, but any unique id will work
+          timestamp - timestamp in whatever format you like, you can use Date.now() if you like
+          title - String
+          body - String
+          author - String
+          category: Any of the categories listed in possible name (categories.js.)
+  
+      GET /posts/:id
+        USAGE:
+          Get the details of a single post
+  
+      POST /posts/:id
+        USAGE:
+          Used for voting on a post
+        PARAMS:
+          option - String: Either "upVote" or "downVote"
+  
+      PUT /posts/:id
+        USAGE:
+          Edit the details of an existing post
+        PARAMS:
+          title - String
+          body - String
+  
+      DELETE /posts/:id
+        USAGE:
+          Sets the deleted flag for a post to 'true'.
+          Sets the parentDeleted flag for all child comments to 'true'.
+  
+      GET /posts/:id/comments
+        USAGE:
+          Get all the comments for a single post
+  
+      POST /comments
+        USAGE:
+          Add a comment to a post
+  
+        PARAMS:
+          id: Any unique ID. As with posts, UUID is probably the best here.
+          timestamp: timestamp. Get this however you want.
+          body: String
+          author: String
+          parentId: Should match a post id in the database.
+  
+      GET /comments/:id
+        USAGE:
+          Get the details for a single comment
+  
+      POST /comments/:id
+        USAGE:
+          Used for voting on a comment.
+        PARAMS:
+          option - String: Either "upVote" or "downVote"
+  
+      PUT /comments/:id
+        USAGE:
+          Edit the details of an existing comment
+  
+        PARAMS:
+          timestamp: timestamp. Get this however you want.
+          body: String
+  
+      DELETE /comments/:id
+        USAGE:
+          Sets a comment's deleted flag to 'true'
+   </pre>
+    `
+    res.send(help)
+})
+
 app.listen(port, () => {
     console.log('Server listening on port %s, Ctrl+C to stop', port)
 })
